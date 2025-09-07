36c7343343c16d7dc8f9e1e1fa31cbc38c0fd26f
CesarCRP97 <ccrp97@hotmail.es>
2025-09-07 14:04:40 +0200

    solved check number of sys.argv

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 
 
 def main():
-    if len(sys) != 2:
+    if len(sys.argv) != 2:
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
     book_report(sys.argv[1])
