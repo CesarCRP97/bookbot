b1d19b32b9cb87dbd38f81d485a6dafd592131c6
CesarCRP97 <ccrp97@hotmail.es>
2025-09-07 13:01:16 +0200

    new method for char counting

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from stats import get_book_text, num_words_text
+from stats import get_book_text, num_words_text, num_chars_in_text
 
 def main():
     text = get_book_text("books/frankenstein.txt")
     num_words = num_words_text(text)
+    char_dict = num_chars_in_text(text)
     print(f"{num_words} words found in the document")
+    print(char_dict)
+
+
 
 
 main()
--- stats.py
+++ stats.py
@@ -9,4 +9,13 @@ def num_words_text(text):
     return len(words)
 
 
-
+def num_chars_in_text(text):
+    char_dictionary = {}
+    lowered_text = text.lower()
+    for c in lowered_text:
+        if c in char_dictionary:
+            char_dictionary[c] += 1
+        else:
+            char_dictionary[c] = 1
+    return char_dictionary
+        
