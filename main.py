43f4ecf0b5be5232d540d904edb2923157abf200
CesarCRP97 <ccrp97@hotmail.es>
2025-09-07 13:47:16 +0200

    add sort method for char_dict & pretty-formatting method

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-from stats import num_words_text, num_chars_in_text
+from stats import num_words_text, num_chars_in_text, sorted_dict_chars
+
+__frank_relative_filepath__ = "books/frankenstein.txt"
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
-    num_words = num_words_text(text)
-    char_dict = num_chars_in_text(text)
-    print(f"{num_words} words found in the document")
-    print(char_dict)
+    book_report(__frank_relative_filepath__)
 
 
 def get_book_text(filepath):
@@ -13,5 +11,23 @@ def get_book_text(filepath):
         file_contents = f.read()
         return file_contents
     
+def pretty_format_chars(sorted_chars):
+    for elem in sorted_chars:
+        if elem["char"].isalpha():
+            print(f"{elem["char"]}: {elem["num"]}")
+    
+def book_report(filepath):
+    text = get_book_text(filepath)
+    num_words = num_words_text(text)
+    num_chars = num_chars_in_text(text)
+    sorted_chars = sorted_dict_chars(num_chars)
+    #Print formatting
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print('----------- Word Count ----------')
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    pretty_format_chars(sorted_chars)
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -12,4 +12,15 @@ def num_chars_in_text(text):
         else:
             char_dictionary[c] = 1
     return char_dictionary
+
+def sort_on(items):
+    return items["char"]
         
+def sorted_dict_chars(unsorted_char_dict):
+    to_sort = []
+    for c in unsorted_char_dict:
+        elem = {"char": c, "num": unsorted_char_dict[c]}
+        to_sort.append(elem)
+
+    to_sort.sort(reverse=False, key=sort_on)
+    return to_sort
